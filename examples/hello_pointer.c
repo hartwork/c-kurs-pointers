97e8aa1f700015c8e95829a59a3ba348e8c8fcbc
Sebastian Pipping <sebastian@pipping.org>
2009-09-14 22:48:16 +0200

    Apply feedback of Friday

--- examples/hello_pointer.c
+++ examples/hello_pointer.c
@@ -5,11 +5,11 @@ main() {
   int i = 3;
   int *p = &i;
 
-  printf("p == %u\n"
-      "*p == %d\n"
-      "i == %d\n", p, *p, i);
+  printf(" p == %#p\n"
+         "*p == %d\n"
+         " i == %d\n", p, *p, i);
 
   *p = 14;
-  printf("i == %d\n", i);
+  printf(" i == %d\n\n", i);
   return 0;
 }
--- examples/my_strlen.c
+++ examples/my_strlen.c
@@ -1,12 +1,9 @@
-#include <assert.h>
-#include <stdlib.h>  /* for NULL */
 #include <stdio.h>
 
 unsigned int
 my_strlen(const char *str) {
   const char *walker = str;
-  assert(str != NULL);
-  while (*walker) {
+  while ('\0' != *walker) {
     walker++;
   }
   return (walker - str);
