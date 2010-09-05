1a316656396c46072c7678b95503bf4405e00d1a
Sebastian Pipping <sebastian@pipping.org>
2010-09-05 22:10:46 +0200

    Add content on call by reference

---
+++ examples/call_by_reference.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void by_value    (int  j) {  j += 3; }
+void by_reference(int* p) { *p += 4; }
+
+int main() {
+  int i = 0;
+
+  by_value(i);
+  by_reference(&i);
+
+  printf("i == %d\n", i);
+
+  return 0;
+}
