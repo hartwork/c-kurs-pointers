cd74c92452527f54c34897ef2850d0f7130c974a
Sebastian Pipping <sebastian@pipping.org>
2010-09-15 15:53:31 +0200

    Improve on hello pointer code sample

--- examples/hello_pointer.c
+++ examples/hello_pointer.c
@@ -6,9 +6,9 @@ int main() {
   int * p = &i;
 
   printf("i == %d, p == %p\n", i, p);
-  T(*p = 0;)
+  T(*p = 4;)
   printf("i == %d, p == %p\n", i, p);
-  T(p = 0;)
+  T(p = 5;)
   printf("i == %d, p == %p\n", i, p);
 
   return 0;
